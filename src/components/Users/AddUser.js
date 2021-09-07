5edd94da9465e529d3067ede000a68a8a9d69672
Tashif <imtashifkudpadi@gmail.com>
2021-09-07 12:26:56 +0530

    Initial commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [formIsValid, setFormIsValid] = useState(true);
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (!enteredUsername.includes("@") || enteredPassword.trim().length === 0) {
+      setFormIsValid(false);
+      return;
+    }
+    if (enteredPassword <= 6) {
+      setFormIsValid(false);
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredPassword("");
+    setFormIsValid(true);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  return (
+    <>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Email</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          {!formIsValid && (
+            <p style={{ color: "red" }}>Please enter a valid email.</p>
+          )}
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="number"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            autoComplete="off"
+          />
+          {!formIsValid && (
+            <p style={{ color: "red" }}>
+              Please enter a password greater than 6.
+            </p>
+          )}
+          <Button type="submit">Submit</Button>
+        </form>
+      </Card>
+    </>
+  );
+};
+
+export default AddUser;
